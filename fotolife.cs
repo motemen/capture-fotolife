566e826616af63948337c904e65a021bf6539e3e
motemen <motemen@gmail.com>
2008-06-09 22:49:46 +0900

    Uploading (fails)

--- fotolife.cs
+++ fotolife.cs
@@ -8,14 +8,19 @@ using System.IO;
 using System.Net;
 using System.Collections.Specialized;
 using System.Text;
+using System.Security.Cryptography;
 
 class CaptureFotolife : Form {
-    //static string FOTOLIFE_POST_URL = "http://f.hatena.ne.jp/{0}/up";
-    static string FOTOLIFE_POST_URL = "http://localhost/";
+    static string HATENA_LOGIN_URL  = "http://www.hatena.ne.jp/login";
+    //static string FOTOLIFE_POST_URL = "http://f.hatena.ne.jp/motemen/up";
+    static string FOTOLIFE_POST_URL = "http://flocal.hatena.ne.jp:3000/motemen/up";
+    //static string FOTOLIFE_POST_URL = "http://localhost/";
 
     Rectangle rectCapture;
     Point ptMouseDown;
     bool isMouseDown = false;
+
+    CookieContainer cookieContainer;
 
     CaptureFotolife() {
         this.TopMost         = true;
@@ -38,42 +43,94 @@ class CaptureFotolife : Form {
         return fileTemp;
     }
 
+    bool LoginHatena(string username, string password) {
+        this.cookieContainer = new CookieContainer();
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(HATENA_LOGIN_URL);
+        request.Method = "POST";
+        request.CookieContainer = this.cookieContainer;
+
+        //WebProxy proxy = new WebProxy();
+        //proxy.Address = new Uri("http://localhost:8080/");
+        //request.Proxy = proxy;
+
+        byte[] requestContent = Encoding.ASCII.GetBytes(
+            String.Format("name={0}&password={1}", username, password)
+        );
+        using (Stream requestStream = request.GetRequestStream()) {
+            requestStream.Write(requestContent, 0, requestContent.Length);
+        }
+
+        request.GetResponse();
+
+        foreach (Cookie c in this.cookieContainer.GetCookies(new Uri("http://f.hatena.ne.jp"))) {
+            Console.WriteLine(c);
+        }
+
+        return true;
+    }
+
     bool UploadFotolife(String filepath) {
         string BOUNDARY = "-----------------------------FOTOLIFEBOUNDARY";
 
+        string rk = "";
+        foreach (Cookie c in this.cookieContainer.GetCookies(new Uri("http://f.hatena.ne.jp"))) {
+            if (c.Name == "rk") {
+                rk = c.Value;
+                break;
+            }
+        }
+        MD5 md5 = MD5.Create();
+        byte[] md5hash = md5.ComputeHash(Encoding.ASCII.GetBytes(rk));
+        string rkm = System.Convert.ToBase64String(md5hash).Replace("=", "");
+        Console.WriteLine(rkm);
+
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(FOTOLIFE_POST_URL);
         request.Method = "POST";
+        request.CookieContainer = this.cookieContainer;
         request.ContentType = String.Format("multipart/form-data; boundary={0}", BOUNDARY);
+
+        //WebProxy proxy = new WebProxy();
+        //proxy.Address = new Uri("http://localhost:8080/");
+        //request.Proxy = proxy;
 
         StringBuilder contentBuilder = new StringBuilder();
         contentBuilder.Append("--").Append(BOUNDARY).Append("\r\n");
-        contentBuilder.Append(String.Format(@"Content-Disposition: form-data; name=""file""; filename=""{0}""", Path.GetFileName(filepath)))
+        contentBuilder.Append(String.Format(@"Content-Disposition: form-data; name=""image1""; filename=""{0}""", Path.GetFileName(filepath)))
                       .Append("\r\n");
         contentBuilder.Append("Content-Type: image/png\r\n");
         contentBuilder.Append("\r\n");
-        using (StreamReader fileReader = new StreamReader(filepath, Encoding.ASCII)) {
-            contentBuilder.Append(fileReader.ReadToEnd());
+        Console.WriteLine(filepath);
+        //using (StreamReader fileReader = new StreamReader(filepath, Encoding.ASCII)) {
+        //    contentBuilder.Append(fileReader.ReadToEnd()).Append("\r\n");
+        //}
+        using (FileStream fileStream = new FileStream(filepath, FileMode.Open)) {
+            int fileSize = (int)fileStream.Length;
+            Console.WriteLine(fileSize);
+            byte[] fileContent = new byte[fileSize];
+            fileStream.Read(fileContent, 0, fileSize);
+            contentBuilder.Append(Encoding.Unicode.GetString(fileContent));
             contentBuilder.Append("\r\n");
         }
+
+        contentBuilder.Append("--").Append(BOUNDARY).Append("\r\n");
+        contentBuilder.Append(@"Content-Disposition: form-data; name=""mode""").Append("\r\n");
+        contentBuilder.Append("\r\n");
+        contentBuilder.Append("enter\r\n");
+
+        contentBuilder.Append("--").Append(BOUNDARY).Append("\r\n");
+        contentBuilder.Append(@"Content-Disposition: form-data; name=""rkm""").Append("\r\n");
+        contentBuilder.Append("\r\n");
+        contentBuilder.Append(rkm).Append("\r\n");
+
         contentBuilder.Append("--").Append(BOUNDARY).Append("--").Append("\r\n");
         byte[] requestContent = Encoding.ASCII.GetBytes(contentBuilder.ToString());
-        Console.WriteLine(contentBuilder);
 
         request.ContentLength = requestContent.Length;
         request.ProtocolVersion = HttpVersion.Version10;
 
         using (Stream requestStream = request.GetRequestStream()) {
             requestStream.Write(requestContent, 0, requestContent.Length);
-        }
-
-        try {
-            using (WebResponse response = request.GetResponse()) {
-                foreach (string key in response.Headers.AllKeys) {
-                  Console.WriteLine("[{0}]\n\t{1}", key, response.Headers[key]);
-                }
-            }
-        } catch (Exception e) {
-            Console.WriteLine(e);
         }
 
         return false;
@@ -105,12 +162,12 @@ class CaptureFotolife : Form {
         if (!isMouseDown)
             return;
 
-        ControlPaint.DrawReversibleFrame(rectCapture, Color.Black, FrameStyle.Dashed);
+        ControlPaint.DrawReversibleFrame(this.RectangleToScreen(rectCapture), Color.Black, FrameStyle.Dashed);
 
         Bitmap bmpCapture   = CaptureScreen();
         String fileCaptured = SaveTemporary(bmpCapture);
-        //UploadFotolife(fileCaptured);
-        UploadFotolife(@"C:\tmp\hoge.hs");
+        UploadFotolife(fileCaptured);
+        //UploadFotolife(@"C:\tmp\hoge.hs");
         Application.Exit();
 
     }
@@ -119,14 +176,14 @@ class CaptureFotolife : Form {
         if (!isMouseDown)
             return;
 
-        ControlPaint.DrawReversibleFrame(rectCapture, Color.Black, FrameStyle.Dashed);
+        ControlPaint.DrawReversibleFrame(this.RectangleToScreen(rectCapture), Color.Black, FrameStyle.Dashed);
 
         rectCapture.Width  = Math.Abs(e.X - ptMouseDown.X);
         rectCapture.Height = Math.Abs(e.Y - ptMouseDown.Y);
         rectCapture.X      = Math.Min(e.X, ptMouseDown.X);
         rectCapture.Y      = Math.Min(e.Y, ptMouseDown.Y);
 
-        ControlPaint.DrawReversibleFrame(rectCapture, Color.Black, FrameStyle.Dashed);
+        ControlPaint.DrawReversibleFrame(this.RectangleToScreen(rectCapture), Color.Black, FrameStyle.Dashed);
     }
 
     protected override void OnPaint(PaintEventArgs e) { }
